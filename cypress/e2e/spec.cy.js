1a34bc72c0b5318afba847f4c7deef2bd81ab4c9
tasyamian <kookietaechopa@gmail.com>
2025-08-30 18:45:59 +0700

    tugas 16 - intercept

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -7,36 +7,28 @@ describe('Fitur Login OrangeHRM', () => {
   it('TC01 - Login with valid credentials', () => {
     cy.get('input[name="username"]').type('Admin');
     cy.get('input[name="password"]').type('admin123');
+    cy.intercept('GET', 'https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index'). as ('login')
     cy.get('button[type="submit"]').click();
-    cy.contains('h6', 'Dashboard').should('be.visible')
+    cy.wait('@login').then((intercept) => expect(intercept.response.statusCode).to.equal(200));
+    //cy.contains('h6', 'Dashboard').should('be.visible')
   })
 
   it('TC02 - Login with invalid username', () => {
     cy.get('input[name="username"]').type('invalid user');
     cy.get('input[name="password"]').type('admin123');
+    cy.intercept('POST', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/validate'). as ('InvalidUsername')
     cy.get('button[type="submit"]').click();
-    cy.contains('p', 'Invalid credentials').should('be.visible')
+    cy.wait('@InvalidUsername').then((intercept) => expect(intercept.response.statusCode).to.equal(302));
+    //cy.contains('p', 'Invalid credentials').should('be.visible')
   })  
 
     it('TC03 - Login with invalid password', () => {
     cy.get('input[name="username"]').type('Admin');
     cy.get('input[name="password"]').type('invalid password');
+    cy.intercept('POST', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/validate'). as ('InvalidPassword')
     cy.get('button[type="submit"]').click();
-    cy.contains('p', 'Invalid credentials').should('be.visible')
-  })
-
-    it('TC04 - Login with username empty', () => {
-    //cy.get('input[name="username"]').type('');
-    cy.get('input[name="password"]').type('admin123');
-    cy.get('button[type="submit"]').click();
-    cy.contains('span', 'Required').should('be.visible')
-  })
-
-    it('TC05 - Login with password empty', () => {
-    cy.get('input[name="username"]').type('Admin');
-    //cy.get('input[name="password"]').type('');
-    cy.get('button[type="submit"]').click();
-    cy.contains('span', 'Required').should('be.visible')
+    cy.wait('@InvalidPassword').then((intercept) => expect(intercept.response.statusCode).to.equal(302));
+    //cy.contains('p', 'Invalid credentials').should('be.visible')
   })
 
 })
